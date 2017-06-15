de5bc8cef4508971310ebd75259c27a544d1e1bb
John Doe <hroch.spb@gmail.com>
2017-06-15 11:57:36 +0200

    Initial commit

---
+++ src/main/java/com/alchesoft/training/jboss/App.java
@@ -0,0 +1,13 @@
+package com.alchesoft.training.jboss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
