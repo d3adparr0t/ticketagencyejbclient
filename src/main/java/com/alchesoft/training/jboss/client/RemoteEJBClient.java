4467ebcea889bf4471c70382c5bed0e1bf00cf90
John Doe <hroch.spb@gmail.com>
2017-06-17 17:28:22 +0200

    added async bean

--- src/main/java/com/alchesoft/training/jboss/client/RemoteEJBClient.java
+++ src/main/java/com/alchesoft/training/jboss/client/RemoteEJBClient.java
@@ -11,6 +11,8 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import java.util.Hashtable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,10 +24,6 @@ public class RemoteEJBClient {
     private static final Hashtable<String, String> jndiProperties = new Hashtable<>();
 
     private static final String MODULE_NAME = "ticket-agency-ejb";
-    /*
-String beanName = NewSessionBean.class.getSimpleName();
-String viewClassName = NewSessionBeanRemote.class.getName();
-     */
 
     public static void main(String[] args) throws Exception {
 
@@ -39,20 +37,54 @@ String viewClassName = NewSessionBeanRemote.class.getName();
         jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
         TheatreInfo info = lookupTheatreInfoEJB();
         TheatreBooker booker = lookupTheatreBookerEJB();
-
+        Future<String> future = null;
         while (true) {
             String command = IOUtils.readLine("> ");
             if ("book".equals(command)) {
                 bookSeat(booker);
+            } else if ("bookasync".equals(command)) {
+                future = bookAsync(booker);
+            } else if ("mail".equals(command)) {
+                receiveMail(future);
             } else if ("list".equals(command)) {
                 log.info(info.printSeats());
             } else if ("quit".equals(command)) {
                 log.info("Bye");
-                break;
+                return;
             } else {
                 log.info("Unknown command " + command);
             }
         }
+    }
+
+    private static void receiveMail(Future<String> future) {
+        if (future== null || !future.isDone()) {
+            log.info("No mail received... be patient.");
+            return;
+        }
+        try {
+            String result = future.get();
+            log.info("Mail received: " + result);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Future<String> bookAsync(TheatreBooker booker) {
+        AtomicInteger seatId = new AtomicInteger(0);
+        try {
+        seatId.set(IOUtils.readInt("Enter seat ID: "));
+        } catch (NumberFormatException nfe) {
+            log.info("Wrong seat ID format");
+            return null;
+        }
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("Booking issued. Please check your mail for further details.");
+        return booker.bookSeatAsync(seatId.get());
     }
 
     private static void bookSeat(TheatreBooker booker) {
